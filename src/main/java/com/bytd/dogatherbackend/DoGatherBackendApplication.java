cefeb11e86405a28616374b26ac191bd1f1100e0
kamil <frankiewiczkamil@gmail.com>
2023-11-09 14:51:39 +0100

    Init project :tada:

---
+++ src/main/java/com/bytd/dogatherbackend/DoGatherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bytd.dogatherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoGatherBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoGatherBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytd/dogatherbackend/DoGatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytd.dogatherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoGatherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
