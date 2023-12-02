e6d49dc3c6fb228938be3dceaf0d5a136c2a6f7c
kamil <frankiewiczkamil@gmail.com>
2023-12-02 18:13:45 +0100

    Add DDL scripts (schema) :card_file_box: :seedling:

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,36 @@
+create table task_list
+(
+    id          UUID primary key,
+    name        varchar(50) not null,
+    description varchar(255),
+    created_at  timestamp default current_timestamp,
+    creator_id  UUID
+);
+
+create table task
+(
+    id           UUID primary key,
+    task_list_id UUID        not null,
+    name         varchar(50) not null,
+    description  varchar(255),
+    created_at   timestamp default current_timestamp,
+    time_spent   int,
+    state        varchar(50),
+    progress     int,
+    creator_id   UUID
+);
+
+create table participant
+(
+    id         UUID primary key,
+    created_at timestamp default current_timestamp
+);
+
+create table permission
+(
+    id             UUID primary key,
+    participant_id UUID        not null,
+    task_list_id   UUID        not null,
+    role           varchar(50) not null,
+    created_at     timestamp default current_timestamp
+);
